ad2cc9798ddb9e9411c6cdc7d134046ad413908b
AfnanKhan59 <123541919+AfnanKhan59@users.noreply.github.com>
2023-01-31 12:48:10 +0500

    IDK WHAT Im DOING LEOL

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<title></title>
 	<link rel="stylesheet" href="style.css">
 
+JINGLE BWELL
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -41,7 +42,7 @@
 		</form>
 	</div>
 </nav>
-BRUH GIT BAD
+
 <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
  </header>
